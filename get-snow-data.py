9d243608fad255432579cf38d8f69e53dd85521e
forsee16 <forsee16@up.edu>
2015-02-25 23:14:19 -0800

    Successful Scrape
    Data is now contained in onthesnow-data.txt.
    Before running get-snow-data.py change file path.

--- get-snow-data.py
+++ get-snow-data.py
@@ -10,55 +10,58 @@
 
 
 
-# Import the libraries necessary to do our work.                                                                                                      
-import urllib2, re
+# Import the libraries necessary to do our work. 
+import string
+try:
+    import urllib.request as urllib2
+except ImportError:
+    import urllib2
+#import urllib2, re
 from bs4 import BeautifulSoup
 
+#Initialize an array of resorts we are looking at
+resorts = ['vail', 'breckenridge', 'mt-hood-meadows', 'mt-baker', 'whistler-blackcomb',
+'alpe-dhuez', 'heavenly-mountain-resort', 'whakapapa', 'killington-resort', 'davos-klosters', 
+'geilo', 'alta-ski-area', 'alyeska-resort', 'valle-nevado', 'las-lenas',
+'laax']
+#Initialize an array of regions related to the resorts
+regions = ['colorado', 'colorado', 'oregon', 'washington', 'british-columbia',
+'northern-alps', 'california', 'new-zealand', 'vermont', 'graubunden',
+'eastern-norway', 'utah', 'alaska', 'chile', 'argentina',
+'graubunden']
 # Open a file to store the .csv data in.                                                                                                              
-f = open('onthesnow-data.txt', 'w')
+f = open('C:\\Users\Ben\Documents\snowfall-data\Group-B1-Projecto-master\onthesnow-data.txt', 'w')
 
-## Loop over all the months                                                                                                                           
-for y in range(2006, 2014):
+for i in range(0,16):
 
+	## Loop over all the months                                                                                                                           
+	for y in range(2006, 2015):
+       
    
-      # Open wunderground.com url for 2009 in this particular iteration.                                                                              
-     # url = "http://www.onthesnow.com/" + region + "/" + resort + "/historical-snowfall.html?&y=" + y
-       url = "http://www.onthesnow.com/" + "colorado" + "/" + "vail" + "/historical-snowfall.html?&y=" + y
-	# Provide helpful message to user as the loop boldly executes forth.
-      print "opening: "
-      print url
-
-      page = urllib2.urlopen(url)
-
-      # Read the one long line of the .html just loaded into page and                                                                                 
-      # store it as a tree of elements within the page.  Thanks crummy!                                                                               
-      soup = BeautifulSoup(page)
-      snowfall = soup.contents[0].contents[1].contents[2].contents[4].contents[0].contents[0].contents[3].contents[1].contents[0].contents[6].contents[0].contents[0].contents[1]
-     # wxs = soup.findAll(attrs={"class":"strong"});
- 
-     # Format month for timestamp, ensuring two digits.                                                                                                                 
-      #if len(str(m)) < 2:
-       # mStamp = '0' + str(m)
-      #else:
-       # mStamp = str(m)
-
-      # Format day for timestamp, ensuring two digits.                                                                                                                  
-      #if len(str(d)) < 2:
-       # dStamp = '0' + str(d)
-      #else:
-       # dStamp = str(d)
-
-      # Build timestamp                                                                                                                            
-      #timestamp = str(2009) + mStamp + dStamp
-
-	# Provide helpful message to user as the loop boldly executes forth.
-      print "writing: "
-      print region + "-" + resort + "-" + y
-      print snowfall#wxs[5].contents[0]
-
-      # Write timestamp and temperature to file                                                                                                    
-      #f.write(region + ',' + resort + ", " + y + ", " + snowfall + '\n')
-      f.write("colorado" + ',' + "vail" + ", " + y + ", " + snowfall + '\n')
-
+		  # Open wunderground.com url for 2009 in this particular iteration.                                                                              
+		  # url = "http://www.onthesnow.com/" + region + "/" + resort + "/historical-snowfall.html?&y=" + y
+		  url = "http://www.onthesnow.com/" + regions[i] + "/" + resorts[i] + "/historical-snowfall.html?&y=" + str(y)
+		  #Provide helpful message to user as the loop boldly executes forth.
+		  print ("opening: ")
+		  print (url + "\n")
+	
+		  page = urllib2.urlopen(url)
+	
+	
+		  # Read the one long line of the .html just loaded into page and                                                                                 
+		  # store it as a tree of elements within the page.  Thanks crummy!                                                                               
+		  soup = BeautifulSoup(page)
+		  snowfall = soup.find(id="view").strong.contents[0]
+		  
+	 
+	
+		  # Provide helpful message to user as the loop boldly executes forth.
+		  print ("writing: ")
+		  print (region + "-" + resort + "-" + y)
+		  print (snowfall + "\n\n\n") 
+	
+		  # Write timestamp and temperature to file                                                                                                    
+		  f.write(regions[i] + ',' + resorts[i] + ", " + str(y) + ", " + snowfall + '\n')
+		  
 # Done getting data! Close file.                                                                                                                   
 f.close()
